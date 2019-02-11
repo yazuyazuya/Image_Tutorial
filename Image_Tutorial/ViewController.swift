eec0fcf229007c57110fc299935bdd658a007226
yazuyazuya <kzyono-1999@docomo.ne.jp>
2019-02-11 14:38:31 +0900

    Initial Commit

---
+++ Image_Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image_Tutorial
+//
+//  Created by 大野和也 on 2019/02/11.
+//  Copyright © 2019 yazuyazuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Image_TutorialTests/Image_TutorialTests.swift
@@ -0,0 +1,34 @@
+//
+//  Image_TutorialTests.swift
+//  Image_TutorialTests
+//
+//  Created by 大野和也 on 2019/02/11.
+//  Copyright © 2019 yazuyazuya. All rights reserved.
+//
+
+import XCTest
+@testable import Image_Tutorial
+
+class Image_TutorialTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
